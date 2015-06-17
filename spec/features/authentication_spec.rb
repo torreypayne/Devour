77625bfa2ccea7fe5cb3cc542191dc9f5ab4fdaa
Torrey Payne <torreyl.payne@gmail.com>
2015-06-17 15:52:50 -0700

    Write More Capybara; get something to raise screen

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -12,11 +12,24 @@ feature "the signup process" do
       visit new_user_url
       fill_in 'email', :with => "bull@shit.com"
       fill_in 'password', :with => "tonsofit"
-      click_on "Create User"
+      click_on "Sign Up!"
     end
 
     scenario "redirects to deck index page after signup" do
       expect(page).to have_content "Your Collection"
+    end
+
+    scenario "renders Shared Decks Index" do
+      expect(page).to have_content "What are you looking to study today?"
+    end
+
+    scenario "can add new decks" do
+      click_on "Add"
+      fill_in 'Title', :with => 'French'
+      fill_in 'Course ID', :with => 9
+      click_on "Public"
+      click_on "Create Your New Deck. Go Study."
+      expect(page).to have_content "French"
     end
   end
 
