a80c209553a6422c2ddd4333a971d1ea4ad5d708
Torrey Payne <torreyl.payne@gmail.com>
2015-06-18 14:34:33 -0700

    Re-introduce 'require_logged_in?' into API controller(prevents fraudulent from non-Users)

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class ApiController < ApplicationController
-    # before_action :require_logged_in!
+    before_action :require_logged_in!
 
-    def require_signed_in!
+    def require_logged_in!
       unless signed_in?
         render json: ["You must be signed in to perform that action!"], status: :unauthorized
       end
