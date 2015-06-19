b501075c21ef98b673e7d0c13757de7bf5c72fdd
Torrey Payne <torreyl.payne@gmail.com>
2015-06-18 17:39:25 -0700

    Allow for two-sided flashcard testing, remove tmp

--- app/assets/javascripts/views/card/CardForm.js
+++ app/assets/javascripts/views/card/CardForm.js
@@ -11,7 +11,12 @@ Devour.Views.CardForm = Backbone.View.extend({
   },
 
   events: {
-    'submit form':'submit'
+    'click .one-sided, .two-sided': 'toggle',
+    'submit form':'submit',
+  },
+
+  toggle: function (attribute) {
+    this.oneSided = !this.oneSided;
   },
 
   template: JST['card/form'],
@@ -19,7 +24,7 @@ Devour.Views.CardForm = Backbone.View.extend({
   errorTemplate: JST['card/error'],
 
   render: function() {
-    this.$el.html(this.template({ card: this.model }));
+    this.$el.html(this.template({ card: this.model, oneSided: this.oneSided }));
     return this;
   },
 
@@ -34,13 +39,22 @@ Devour.Views.CardForm = Backbone.View.extend({
 
         }
         cardForm.deck.cards().add(card);
+        if ($('.two-sided').hasClass('btn-primary')) {
+          var newQuestion = cardData.card.answer;
+          var newAnswer = cardData.card.question;
+          var otherSide = new Devour.Models.Card({
+            question: newQuestion,
+            answer: newAnswer,
+            deck_id: cardData.card.deck_id
+          });
+          otherSide.save({});
+        }
         cardForm.render();
       },
       error: function(response) {
         $('div.has-error').append($('<p>').text(response));
       }
     });
-
   }
 
 });
