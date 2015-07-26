d186906b7ee2b844feb14bc26bd9ff7005bebeb8
Torrey Payne <torreyl.payne@gmail.com>
2015-07-26 00:16:06 -0700

    Change front-end theme

--- app/assets/javascripts/views/forum/RedditIndex.js
+++ app/assets/javascripts/views/forum/RedditIndex.js
@@ -13,8 +13,13 @@ Devour.Views.RedditIndex = Backbone.CompositeView.extend({
 
     render: function() {
         console.log('rendered');
-        this.$el.html(this.template({ forum: this.collection }));
-        console.log(this.collection);
+        if (this._currentView) {
+          this.attachSubviews();
+          // this._currentView.render();
+        } else {
+          this.$el.html(this.template({ forum: this.collection }));
+          console.log(this.collection);
+        }
         return this;
     },
 
@@ -22,23 +27,19 @@ Devour.Views.RedditIndex = Backbone.CompositeView.extend({
       event.preventDefault();
       var sub = $(event.currentTarget);
       var id = sub.data('id');
-      var subReddit = this.collection.getOrFetch(id);
+      this.subReddit = this.collection.getOrFetch(id);
       var subView = new Devour.Views.RedditShow({
-        model: subReddit,
+        model: this.subReddit,
         collection: this.collection
       });
       this.swapReddit(subView);
     },
 
     swapReddit: function(view) {
-      if (this._currentView) {
-        this.removeSubview(this._currentView);
-      }
       this._currentView && this._currentView.remove();
       this._currentView = view;
       this.addSubview('.forum-index', this._currentView);
       this.render();
-      return view;
     },
 
 
